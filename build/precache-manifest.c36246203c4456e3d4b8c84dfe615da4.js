3a5da03ea65a5059570b2bcbb82aa3ac44300517
Your Namesparshs51 <sparshs51@gmail.com>
2020-09-30 22:48:10 +0530

    final pus

---
+++ build/precache-manifest.c36246203c4456e3d4b8c84dfe615da4.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679521e54a928a6b4572a8355448f498",
+    "url": "/college-app/index.html"
+  },
+  {
+    "revision": "8d6fa7bb513076f1326e",
+    "url": "/college-app/static/css/main.551529b0.chunk.css"
+  },
+  {
+    "revision": "bea447457f1a20976ecc",
+    "url": "/college-app/static/js/2.b61a3f18.chunk.js"
+  },
+  {
+    "revision": "349a1a8e960b3e799f5df22fa2e8de3a",
+    "url": "/college-app/static/js/2.b61a3f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6fa7bb513076f1326e",
+    "url": "/college-app/static/js/main.d8e81f36.chunk.js"
+  },
+  {
+    "revision": "015087d830d8a30ac91c",
+    "url": "/college-app/static/js/runtime-main.34049ec2.js"
+  },
+  {
+    "revision": "26790dcdb56da3868ccaae2649fe16ce",
+    "url": "/college-app/static/media/DIN Pro 400.26790dcd.otf"
+  },
+  {
+    "revision": "748540994dbf8d8f9e84553d4ee971b3",
+    "url": "/college-app/static/media/DIN Pro Black 900.74854099.otf"
+  },
+  {
+    "revision": "ac34cae9fd43fd6c0b38c2b1d7aca3b2",
+    "url": "/college-app/static/media/DIN Pro Black Italic 900.ac34cae9.otf"
+  },
+  {
+    "revision": "37684a0a69c0bf7152f34ed934dc07a6",
+    "url": "/college-app/static/media/DIN Pro Bold 700.37684a0a.otf"
+  },
+  {
+    "revision": "5c42c7eaf714146c98012c9e1e60a573",
+    "url": "/college-app/static/media/DIN Pro Bold Italic 700.5c42c7ea.otf"
+  },
+  {
+    "revision": "2e36241f4b446011b1820e269bfd829a",
+    "url": "/college-app/static/media/DIN Pro Condensed Black 900.2e36241f.otf"
+  },
+  {
+    "revision": "70c3ca4daab67b49fbe806be28b840d8",
+    "url": "/college-app/static/media/DIN Pro Condensed Black Italic 900.70c3ca4d.otf"
+  },
+  {
+    "revision": "481d38e7ec5bb0a63bf63224798b9949",
+    "url": "/college-app/static/media/DIN Pro Condensed Bold 700.481d38e7.otf"
+  },
+  {
+    "revision": "eae6353863379d2ad11a9ddd90c2d19a",
+    "url": "/college-app/static/media/DIN Pro Condensed Bold Italic 700.eae63538.otf"
+  },
+  {
+    "revision": "8c829deba3fb6d4f71425499f5e0685d",
+    "url": "/college-app/static/media/DIN Pro Condensed Italic 400.8c829deb.otf"
+  },
+  {
+    "revision": "5c04b39551c889f7ce09fbab7bfcf8d1",
+    "url": "/college-app/static/media/DIN Pro Condensed Light 300.5c04b395.otf"
+  },
+  {
+    "revision": "b22bc4c2220767eec9a260012de79cb2",
+    "url": "/college-app/static/media/DIN Pro Condensed Light Italic 300.b22bc4c2.otf"
+  },
+  {
+    "revision": "bf27039ecba4e6511abddd84122a8aac",
+    "url": "/college-app/static/media/DIN Pro Condensed Medium 500.bf27039e.otf"
+  },
+  {
+    "revision": "a21f3537ed6de73e328e72f0c0dd3a39",
+    "url": "/college-app/static/media/DIN Pro Condensed Medium Italic 500.a21f3537.otf"
+  },
+  {
+    "revision": "9b3413ea2eb0e67d54fa8aea61391393",
+    "url": "/college-app/static/media/DIN Pro Condensed Regular 400.9b3413ea.otf"
+  },
+  {
+    "revision": "26ee448575d02e41d48c82154c130485",
+    "url": "/college-app/static/media/DIN Pro Italic 400.26ee4485.otf"
+  },
+  {
+    "revision": "001e05afb3ea98d66704389529dda7bd",
+    "url": "/college-app/static/media/DIN Pro Light 300.001e05af.otf"
+  },
+  {
+    "revision": "f51d93b3c9937dcc8a25809fd44081ff",
+    "url": "/college-app/static/media/DIN Pro Light Italic 300.f51d93b3.otf"
+  },
+  {
+    "revision": "90d4bd83166cc1a605599f5551a02cf6",
+    "url": "/college-app/static/media/DIN Pro Medium 500.90d4bd83.otf"
+  },
+  {
+    "revision": "a6489cbe1ca9b0824a2d055a36bc628b",
+    "url": "/college-app/static/media/DIN Pro Medium Italic 500.a6489cbe.otf"
+  },
+  {
+    "revision": "e13e0eafe69e5a0dd5954400f3a13e9a",
+    "url": "/college-app/static/media/README.e13e0eaf.txt"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/college-app/static/media/college.619ee97e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/college-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
